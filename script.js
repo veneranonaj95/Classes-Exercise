9ac82f584d980b2a5f52b63042938a5b08d71d77
Venera Nonaj <124005234+veneranonaj95@users.noreply.github.com>
2023-04-01 18:47:37 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+//OO Challenge
+//Part 1
+let myFirstVehicle = new Vehicle("Honda", "Monster Truck", 1999);
+myFirstVehicle.honk(); // "Beep."
+
+class Vehicle {
+    constructor(make, model, year) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+    }
+  
+    honk() {
+      return "Beep.";
+    }
+  
+    toString() {
+      return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
+    }
+  }
+
+  //Part 2
+let myFirstCar = new Car("Toyota", "Corolla", 2005);
+myFirstCar.toString(); // "The vehicle is a Toyota Corolla from 2005."
+myFirstCar.honk();     // "Beep."
+myFirstCar.numWheels;  // 4
+
+
+class Car extends Vehicle {
+    constructor(make, model, year) {
+      super(make, model, year);
+      this.numWheels = 4;
+    }
+  }
+
+//Part 3
+let myFirstMotorcycle = new Motorcycle("Honda", "Nighthawk", 2000);
+
+myFirstMotorcycle.toString();
+// "The vehicle is a Honda Nighthawk from 2000."
+
+myFirstMotorcycle.honk();     // "Beep."
+myFirstMotorcycle.revEngine(); // "VROOM!!!"
+myFirstMotorcycle.numWheels;  // 2
+
+class Motorcycle extends Vehicle {
+    constructor(make, model, year) {
+      super(make, model, year);
+      this.numWheels = 2;
+    }
+  
+    revEngine() {
+      return "VROOM!!!";
+    }
+  }
+
+//Part 4
+let garage = new Garage(2);
+garage.vehicles; // []
+garage.add(new Car("Hyundai", "Elantra", 2015)); // "Vehicle added!"
+garage.vehicles; // [Car]
+garage.add("Taco"); // "Only vehicles are allowed in here!"
+
+garage.add(new Motorcycle("Honda", "Nighthawk", 2000));
+// "Vehicle added!"
+garage.vehicles; // [Car, Motorcycle]
+
+garage.add(new Motorcycle("Honda", "Nighthawk", 2001));
+// "Sorry, we're full."
+
+class Garage {
+    constructor(capacity) {
+      this.vehicles = [];
+      this.capacity = capacity;
+    }
+  
+    add(newVehicle) {
+      if (!(newVehicle instanceof Vehicle)) {
+        return "Only vehicles are allowed in here!";
+      }
+      if (this.vehicles.length >= this.capacity) {
+        return "Sorry, we're full.";
+      }
+      this.vehicles.push(newVehicle);
+      return "Vehicle added!";
+    }
+  }
+
+  
